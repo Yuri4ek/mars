58edf9f4ad93816eac7c8a5bf494f0950b196ba3
Yuri4ek <dinourski@gmail.com>
2025-02-25 15:02:53 +0300

    урок1доп2

--- Server.py
+++ Server.py
@@ -246,29 +246,82 @@ def pepe():
                     <html lang="en">
                       <head>
                         <meta charset="utf-8">
-                        <link rel="stylesheet" 
-                        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
-                        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
-                        crossorigin="anonymous">
+                        <meta
+                            name="viewport"
+                            content="width=device-width, initial-scale=1"
+                        />
+                        <link
+                            rel="stylesheet"
+                            href=
+                "https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css"
+                        />
+                        <script src=
+                "https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js">
+                        </script>
+                        <script src=
+                "https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js">
+                
+                        </script>
                         <title>Привет, Марс!</title>
                       </head>
                       <body>
                         <h1>Пейзажи марса!!!!!!!!!</h1>
-                        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-                          <div class="carousel-inner">
-                            <div class="carousel-item active">
-                              <img src="
-{url_for('static', filename='img/pepe1.png')}" class="d-block w-100" alt="...">
+                        <div
+                            id="myCarousel"
+                            class="carousel slide"
+                            data-ride="carousel"
+                        >
+                            <!-- Indicators -->
+                            <ol class="carousel-indicators">
+                                <li
+                                    data-target="#myCarousel"
+                                    data-slide-to="0"
+                                    class="active"
+                                ></li>
+                                <li
+                                    data-target="#myCarousel"
+                                    data-slide-to="1"
+                                ></li>
+                                <li
+                                    data-target="#myCarousel"
+                                    data-slide-to="2"
+                                ></li>
+                            </ol>
+                            <!-- Wrapper for slides -->
+                            <div class="carousel-inner">
+                                <div class="item active">
+                                    <img src="static/img/pepe1.png"/>
+                                </div>
+                                <div class="item">
+                                    <img src="static/img/pepe2.png"/>
+                                </div>
+                                <div class="item">
+                                    <img src="static/img/pepe3.png"/>
+                                </div>
                             </div>
-                            <div class="carousel-item active">
-                              <img src="
-{url_for('static', filename='img/pepe2.png')}" class="d-block w-100" alt="...">
-                            </div>
-                            <div class="carousel-item">
-                              <img src="
-{url_for('static', filename='img/pepe3.png')}" class="d-block w-100" alt="...">
-                            </div>
-                          </div>
+                            <!-- Left and right controls -->
+                            <a
+                                class="left carousel-control"
+                                href="#myCarousel"
+                                data-slide="prev"
+                            >
+                                <span
+                                    class="glyphicon glyphicon-chevron-left"
+                                ></span>
+                                <span class="sr-only"
+                                    >Previous</span
+                                >
+                            </a>
+                            <a
+                                class="right carousel-control"
+                                href="#myCarousel"
+                                data-slide="next"
+                            >
+                                <span
+                                    class="glyphicon glyphicon-chevron-right"
+                                ></span>
+                                <span class="sr-only">Next</span>
+                            </a>
                         </div>
                       </body>
                     </html>'''
