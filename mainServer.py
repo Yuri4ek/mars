200ddf9981d9e76ba5c1015fdc52ac8b2153971c
Yuri4ek <dinourski@gmail.com>
2025-03-13 22:02:00 +0300

    очистка

--- mainServer.py
+++ mainServer.py
@@ -87,61 +87,6 @@ def index():
     return render_template("index.html", jobs=jobs)
 
 
-if True:
-    @app.route('/distribution')
-    def distribution():
-        data = ['Mike', 'Юра Борисов', 'Jason Statham', 'Я']
-        return render_template('distribution.html',
-                               data=data)
-
-
-    @app.route('/table/<sex>/<year>')
-    def room(sex, year):
-        year = int(year)
-        return render_template('room.html',
-                               sex=sex, year=year)
-
-
-    @app.route('/training/<prof>')
-    def training(prof):
-        return render_template('training.html',
-                               title=prof)
-
-
-    @app.route('/list_prof/<key>')
-    def list_prof(key):
-        list_prof = ['цветоед', 'инженер', 'строитель', 'врач', 'магистр']
-        return render_template('list_prof.html',
-                               title=key, list_prof=list_prof)
-
-
-    @app.route('/answer')
-    def answer():
-        data = {'title': 'Анкета',
-                'surname': 'Pedrov',
-                'name': 'Mark',
-                'education': 'Выше Высокого',
-                'profession': 'Врач',
-                'sex': 'male',
-                'motivation': 'Захотел',
-                'ready': 'Всегда'}
-        return render_template('auto_answer.html',
-                               data=data)
-
-
-    @app.route('/auto_answer')
-    def auto_answer():
-        data = {'title': 'Анкета',
-                'surname': 'Петров',
-                'name': 'Маркс',
-                'education': 'Выше среднего',
-                'profession': 'Глав Врач',
-                'sex': 'male',
-                'motivation': 'Захотел и захотел',
-                'ready': 'Всегда и никогда'}
-        return render_template('auto_answer.html',
-                               data=data)
-
 if __name__ == '__main__':
     main()
     app.run(debug=True, port=8080, host='127.0.0.1')
