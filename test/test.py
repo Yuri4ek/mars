59f0c7ec46c980b2805d8ce6abd57e94f64cb431
Yuri4ek <dinourski@gmail.com>
2025-03-20 22:57:23 +0300

    домашка 3-4

--- test/test.py
+++ test/test.py
@@ -1,4 +1,4 @@
-from requests import get, post, delete
+from requests import get, post, delete, put
 from pprint import pprint
 
 # пустой json
@@ -19,6 +19,31 @@ print(post('http://localhost:8080/api/jobs',
 print(delete('http://localhost:8080/api/jobs/999').json())
 print(delete('http://localhost:8080/api/jobs/e').json())
 print(delete('http://localhost:8080/api/jobs/1').json())
+
+print(put('http://localhost:8080/api/jobs/2',
+          json={}).json())
+print(put('http://localhost:8080/api/jobs/у',
+          json={'job': 'job1',
+                'work_size': 12,
+                'team_leader': 1,
+                'collaborators': '1,2',
+                'is_finished': False}).json())
+print(put('http://localhost:8080/api/jobs/999',
+          json={'job': 'job1',
+                'work_size': 12,
+                'team_leader': 1,
+                'collaborators': '1,2',
+                'is_finished': False}).json())
+print(put('http://localhost:8080/api/jobs/2',
+          json={'job': 'job1',
+                'collaborators': '1,2',
+                'is_finished': False}).json())
+print(put('http://localhost:8080/api/jobs/2',
+          json={'job': 'job1',
+                'work_size': 12,
+                'team_leader': 1,
+                'collaborators': '1,2',
+                'is_finished': False}).json())
 
 print(get('http://localhost:8080/api/jobs').json())
 print(get('http://localhost:8080/api/jobs/1').json())
